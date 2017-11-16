4f24c5742ae5e5d0c1838b2a01bbabd76cf29ea7
tjy-tool <990423909@qq.com>
2017-11-16 18:43:16 +0800

    select_ftp done

--- SELECT_FTP/server/select_server.py
+++ SELECT_FTP/server/select_server.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-# Funtion:      
+# Funtion:
 # Filename:
 
-import select, socket, queue, json
-import os, hashlib
+import select
+import socket
+import queue
+import json
+import os
+import hashlib
+
 
 class Select_server(object):
-    def __init__(self, HOST,PORT):
+    def __init__(self, HOST, PORT):
         self.server = socket.socket()
         self.server.bind((HOST, PORT))
         self.server.listen(1000)
@@ -30,23 +35,27 @@ class Select_server(object):
         #              'recv_cmd': None,
         #              'send_cmd': Info_cmd,
         #              'file_obj': send_file_obj 或 recv_file_obj
+        #     }
         # }
 
     def run(self):
         print('server is running...')
         while True:
-            readable, writeable, exceptional = select.select(self.inputs, self.outputs, self.inputs)
+            readable, writeable, exceptional = select.select(
+                self.inputs, self.outputs, self.inputs)
             for r in readable:
-                if r is self.server:    #每个r就是一个socket
+                if r is self.server:  # 每个r就是一个socket
                     conn, client_addr = r.accept()
                     conn.setblocking(False)
                     print('来了个新链接: ', client_addr)
                     self.inputs.append(conn)
 
-                    self.conn_info_dict[conn] = { }
+                    self.conn_info_dict[conn] = {}
                     # self.conn_info_dict[conn]['conn'] = conn
-                    self.conn_info_dict[conn]['q_recv_data'] = queue.Queue()       # conn_info_dict[0]
-                    self.conn_info_dict[conn]['first_recv'] = 0         # 0 代表首次接收，即接受命令, 1 表示接收数据， 2 表示发送数据
+                    # conn_info_dict[0]
+                    self.conn_info_dict[conn]['q_recv_data'] = queue.Queue()
+                    # 0 代表首次接收，即接受命令, 1 表示接收数据， 2 表示发送数据
+                    self.conn_info_dict[conn]['first_recv'] = 0
 
                 else:    # r不是server的话,那就只能是一个与客户端建立的连接的fd了
                     if self.conn_info_dict[r]['first_recv'] == 0:
@@ -79,17 +88,20 @@ class Select_server(object):
                             finally:
                                 pass
 
-                                self.conn_info_dict[r]['recv_cmd'] = cmd_dict  # 将命令接收进去，为conn_info_dict[2]
+                                # 将命令接收进去，为conn_info_dict[2]
+                                self.conn_info_dict[r]['recv_cmd'] = cmd_dict
 
                                 if cmd_dict['func'] == 'push':      # 接收文件命令
                                     info_dict = {
                                         'info': 'Ready to recv'
                                     }
                                     if self.conn_info_dict[r]['recv_cmd']['file_size'] != 0:
-                                        self.conn_info_dict[r]['first_recv'] = 1  # 1 表示接下来要接收数据
+                                        # 1 表示接下来要接收数据
+                                        self.conn_info_dict[r]['first_recv'] = 1
                                         self.conn_info_dict[r]['residue_recv_size'] = cmd_dict['file_size']
 
-                                    r.send(json.dumps(info_dict).encode('utf-8'))  # 开始接收文件
+                                    r.send(json.dumps(info_dict).encode(
+                                        'utf-8'))  # 开始接收文件
 
                                 if cmd_dict['func'] == 'pull':      # 发送文件命令
                                     file_name = cmd_dict['file_name']
@@ -104,17 +116,18 @@ class Select_server(object):
                                         self.conn_info_dict[r]['send_cmd'] = info_dict
                                         self.conn_info_dict[r]['first_recv'] = 2
                                         self.conn_info_dict[r]['residue_send_size'] = \
-                                            os.stat(self.conn_info_dict[r]['send_cmd']['file_name']).st_size
+                                            os.stat(
+                                                self.conn_info_dict[r]['send_cmd']['file_name']).st_size
                                         self.outputs.append(r)
                                     else:
                                         info_dict = {
                                             'info': 'File dose not Exist'
                                         }
 
-                                    r.send(json.dumps(info_dict).encode('utf-8'))  # 开始发送文件
+                                    r.send(json.dumps(info_dict).encode(
+                                        'utf-8'))  # 开始发送文件
                                     # print(' seond ok', info_dict)
                                     # print('conn_info_dict', self.conn_info_dict)
-
 
                     elif self.conn_info_dict[r]['first_recv'] == 1:      # 1 表示接收数据
                         data = r.recv(1024)
@@ -122,11 +135,14 @@ class Select_server(object):
                         # self.conn_info_dict[r]['q_recv_data'].put(data)
                         if self.conn_info_dict[r]['residue_recv_size'] == self.conn_info_dict[r]['recv_cmd']['file_size']:
                             file_name = self.conn_info_dict[r]['recv_cmd']['file_name']
-                            self.conn_info_dict[r]['file_obj'] = open(file_name, 'wb')
-                            self.conn_info_dict[r]['recv_file_md5'] = hashlib.md5()
+                            self.conn_info_dict[r]['file_obj'] = open(
+                                file_name, 'wb')
+                            self.conn_info_dict[r]['recv_file_md5'] = hashlib.md5(
+                            )
 
                         self.conn_info_dict[r]['file_obj'].write(data)
-                        self.conn_info_dict[r]['residue_recv_size'] -= len(data)
+                        self.conn_info_dict[r]['residue_recv_size'] -= len(
+                            data)
                         self.conn_info_dict[r]['recv_file_md5'].update(data)
                         if self.conn_info_dict[r]['residue_recv_size'] == 0:
                             # recved_md5 = r.recv(1024)
@@ -144,14 +160,16 @@ class Select_server(object):
                 # print('In w')
                 if self.conn_info_dict[w]['residue_send_size'] == self.conn_info_dict[w]['send_cmd']['file_size']:
                     self.conn_info_dict[w]['send_file_md5'] = hashlib.md5()
-                    self.conn_info_dict[w]['file_obj'] = open(self.conn_info_dict[w]['send_cmd']['file_name'], 'rb')
+                    self.conn_info_dict[w]['file_obj'] = open(
+                        self.conn_info_dict[w]['send_cmd']['file_name'], 'rb')
                 line = self.conn_info_dict[w]['file_obj'].readline()
                 # print('w',self.conn_info_dict[w], 'line', line)
                 w.send(line)
                 self.conn_info_dict[w]['residue_send_size'] -= len(line)
                 self.conn_info_dict[w]['send_file_md5'].update(line)
                 if self.conn_info_dict[w]['residue_send_size'] == 0:    # 发送完了文件
-                    md5_hex = self.conn_info_dict[w]['send_file_md5'].hexdigest()
+                    md5_hex = self.conn_info_dict[w]['send_file_md5'].hexdigest(
+                    )
                     # print('-----', md5_hex)
                     w.send(md5_hex.encode('utf-8'))
 
@@ -165,12 +183,12 @@ class Select_server(object):
                 del self.conn_info_dict[e]
                 e.close()
 
-
     # def push(self):
     #     pass
     #
     # def pull(self, Recv_dict):
     #     pass
+
 
 if __name__ == '__main__':
     sel_server = Select_server('localhost', 9999)
